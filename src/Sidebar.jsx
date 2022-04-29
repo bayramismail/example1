c8210d81257fe75ad1c751fcef670eb7fecf9289
bayramismail <83179561+bayramismail@users.noreply.github.com>
2022-04-30 02:56:51 +0300

    güncelle

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 
 const Sidebar = (props) => {
+  const [islem, setIslem] = useState("topla");
   return (
-    <div style={{backgroundColor:"red"}}>
+    <div style={{ backgroundColor: "red" }}>
       <h1>Sidebar</h1>
-      <button onClick={()=>{
-        props.setCount(0)
-      }}>Sıfırla</button>
+      <button
+        onClick={() => {
+          props.setCount(0);
+        }}
+      >
+        Sıfırla
+      </button>
 
-      {props.sayiListesi.map((value,i) => (
-        <button key={i} onClick={() => props.artırParams(value.value)}>{value.text}</button>
+      {props.sayiListesi.map((value, i) => (
+        <button key={i} onClick={() => props.artırParams(value.value)}>
+          {value.text}
+        </button>
       ))}
-      <br/>
-      {props.sayiListesi.map((value,i) => (
-        <button key={i} onClick={() => props.artırParams(-value.value)}>{value.text}</button>
-      ))}
+      <br />
+      <button onClick={() => {}}>Çıkar</button>
     </div>
   );
 };
 
 export default Sidebar;
-
